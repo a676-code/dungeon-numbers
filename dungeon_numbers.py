fc9a13889f52aa60d349a5f2209cc2037bfe6c55
thisusernameisaparadox <109701008+thisusernameisaparadox@users.noreply.github.com>
2022-12-15 19:32:15 -0500

    initial commit

--- dungeon_numbers.py
+++ dungeon_numbers.py
@@ -4,7 +4,7 @@ sys.path.append("C:\\Users\\\\AppData\\Local\\Programs\\Python\\Python311\\Lib\\
 
 import numpy as np
 
-def dungeonNumber(a, b):
+def base(a, b):
     result = 0
     digits_backward = [int(d) for d in str(a)]
     digits = np.flip(digits_backward)
@@ -14,54 +14,54 @@ def dungeonNumber(a, b):
 
 # Descending from the bottom up
 # 11
-print(dungeonNumber(10, 11))
+print(base(10, 11))
 # 13
-print(dungeonNumber(10, dungeonNumber(11, 12)))
+print(base(10, base(11, 12)))
 # 16
-print(dungeonNumber(10, dungeonNumber(11, dungeonNumber(12, 13))))
+print(base(10, base(11, base(12, 13))))
 # 20
-print(dungeonNumber(10, dungeonNumber(11, dungeonNumber(12, dungeonNumber(13, 14)))))
+print(base(10, base(11, base(12, base(13, 14)))))
 # 25
-print(dungeonNumber(10, dungeonNumber(11, dungeonNumber(12, dungeonNumber(13, dungeonNumber(14, 15))))))
+print(base(10, base(11, base(12, base(13, base(14, 15))))))
 
 print()
 
 # Ascending from the bottom up
 # 11
-print(dungeonNumber(11, 10))
+print(base(11, 10))
 # 13
-print(dungeonNumber(dungeonNumber(10, 11), 12))
+print(base(base(10, 11), 12))
 # 16
-print(dungeonNumber(dungeonNumber(dungeonNumber(10, 11), 12), 13))
+print(base(base(base(10, 11), 12), 13))
 # 20
-print(dungeonNumber(dungeonNumber(dungeonNumber(dungeonNumber(10, 11), 12), 13), 14))
+print(base(base(base(base(10, 11), 12), 13), 14))
 # 30
-print(dungeonNumber(dungeonNumber(dungeonNumber(dungeonNumber(dungeonNumber(10, 11), 12), 13), 14), 15))
+print(base(base(base(base(base(10, 11), 12), 13), 14), 15))
 
 print()
 
 # Descending from the bottom up
 #11
-print(dungeonNumber(11, 10))
+print(base(11, 10))
 # 13
-print(dungeonNumber(12, dungeonNumber(11, 10)))
+print(base(12, base(11, 10)))
 # 16
-print(dungeonNumber(13, dungeonNumber(12, dungeonNumber(11, 10))))
+print(base(13, base(12, base(11, 10))))
 # 20
-print(dungeonNumber(14, dungeonNumber(13, dungeonNumber(12, dungeonNumber(11, 10)))))
+print(base(14, base(13, base(12, base(11, 10)))))
 # 25
-print(dungeonNumber(15, dungeonNumber(14, dungeonNumber(13, dungeonNumber(12, dungeonNumber(11, 10))))))
+print(base(15, base(14, base(13, base(12, base(11, 10))))))
 
 print()
 
 # Descending from the top down
 # 11
-print(dungeonNumber(11, 10))
+print(base(11, 10))
 # 13
-print(dungeonNumber(dungeonNumber(12, 11), 10))
+print(base(base(12, 11), 10))
 # 16
-print(dungeonNumber(dungeonNumber(dungeonNumber(13, 12), 11), 10))
+print(base(base(base(13, 12), 11), 10))
 # 20
-print(dungeonNumber(dungeonNumber(dungeonNumber(dungeonNumber(14, 13), 12), 11), 10))
+print(base(base(base(base(14, 13), 12), 11), 10))
 # 28
-print(dungeonNumber(dungeonNumber(dungeonNumber(dungeonNumber(dungeonNumber(15, 14), 13), 12), 11), 10))
+print(base(base(base(base(base(15, 14), 13), 12), 11), 10))
