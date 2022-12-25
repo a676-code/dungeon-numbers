7f21b48aa26fea41a0fe064d07cda1443cc6db83
thisusernameisaparadox <109701008+thisusernameisaparadox@users.noreply.github.com>
2022-12-25 15:48:06 -0500

    fully implemented

--- dungeon_numbers.py
+++ dungeon_numbers.py
@@ -8,67 +8,69 @@ def base(a, b):
         result += d * (int(b) ** i)
     return result
 
-def bottomUp(*args):
-    num = base(args[len(args) - 2], args[len(args) - 1])
-    args = list(args)
-    if (args):
-        args.pop(len(args) - 1)
-        if (len(args) > 1):
-            args.pop(len(args) - 1)
-            return bottomUp(*tuple(args), num)
-        else:
-            return num
-        
-def topDown(*args):
-    if len(args) == 1:
-        return args[0]
-    else:
-        num = base(args[0], args[1])
+def dungeonNumber(*args, mode='b'):
+    if mode == 'b':
+        num = base(args[len(args) - 2], args[len(args) - 1])
         args = list(args)
         if (args):
-            args.pop(0)
+            args.pop(len(args) - 1)
             if (len(args) > 1):
-                args.pop(0)
-                return topDown(num, *tuple(args))
+                args.pop(len(args) - 1)
+                return dungeonNumber(*tuple(args), num, mode='b')
             else:
                 return num
+    elif mode == 't':
+        if len(args) == 1:
+            return args[0]
+        else:
+            num = base(args[0], args[1])
+            args = list(args)
+            if (args):
+                args.pop(0)
+                if (len(args) > 1):
+                    args.pop(0)
+                    return dungeonNumber(num, *tuple(args), mode='t')
+                else:
+                    return num
+    else:
+        print("ERROR: unknown mode specified")
 
-print(bottomUp(10), end=', ') # 10
-print(bottomUp(10, 11), end=', ') # 11
-print(bottomUp(10, 11, 12), end=', ') # 13
-print(bottomUp(10, 11, 12, 13), end=', ') # 16
-print(bottomUp(10, 11, 12, 13, 14), end=', ') # 20
-print(bottomUp(10, 11, 12, 13, 14, 15), end=', ') # 25
-print(bottomUp(10, 11, 12, 13, 14, 15, 16), end=', ') # 31
-print(bottomUp(10, 11, 12, 13, 14, 15, 16, 17), end=', ') # 38
-print(bottomUp(10, 11, 12, 13, 14, 15, 16, 17, 18)) # 46
+print(dungeonNumber(10), end=', ') # 10
+print(dungeonNumber(10, 11), end=', ') # 11
+print(dungeonNumber(10, 11, 12), end=', ') # 13
+print(dungeonNumber(10, 11, 12, 13), end=', ') # 16
+print(dungeonNumber(10, 11, 12, 13, 14), end=', ') # 20
+print(dungeonNumber(10, 11, 12, 13, 14, 15), end=', ') # 25
+print(dungeonNumber(10, 11, 12, 13, 14, 15, 16), end=', ') # 31
+print(dungeonNumber(10, 11, 12, 13, 14, 15, 16, 17), end=', ') # 38
+print(dungeonNumber(10, 11, 12, 13, 14, 15, 16, 17, 18)) # 46
 
-print(bottomUp(10), end=', ') # 10
-print(bottomUp(11, 10), end=', ') # 11
-print(bottomUp(12, 11, 10), end=', ') # 13
-print(bottomUp(13, 12, 11, 10), end=', ') # 16
-print(bottomUp(14, 13, 12, 11, 10), end=', ') # 20
-print(bottomUp(15, 14, 13, 12, 11, 10), end=', ') # 25
-print(bottomUp(16, 15, 14, 13, 12, 11, 10), end=', ') # 31
-print(bottomUp(17, 16, 15, 14, 13, 12, 11, 10), end=', ') # 38
-print(bottomUp(18, 17, 16, 15, 14, 13, 12, 11, 10)) # 46
+print(dungeonNumber(10), end=', ') # 10
+print(dungeonNumber(11, 10), end=', ') # 11
+print(dungeonNumber(12, 11, 10), end=', ') # 13
+print(dungeonNumber(13, 12, 11, 10), end=', ') # 16
+print(dungeonNumber(14, 13, 12, 11, 10), end=', ') # 20
+print(dungeonNumber(15, 14, 13, 12, 11, 10), end=', ') # 25
+print(dungeonNumber(16, 15, 14, 13, 12, 11, 10), end=', ') # 31
+print(dungeonNumber(17, 16, 15, 14, 13, 12, 11, 10), end=', ') # 38
+print(dungeonNumber(18, 17, 16, 15, 14, 13, 12, 11, 10)) # 46
 
-print(topDown(10), end=', ') # 10
-print(topDown(10, 11), end=', ') # 11
-print(topDown(10, 11, 12), end=', ') # 13
-print(topDown(10, 11, 12, 13), end=', ') # 16
-print(topDown(10, 11, 12, 13, 14), end=', ') # 20 
-print(topDown(10, 11, 12, 13, 14, 15), end=', ') # 30
-print(topDown(10, 11, 12, 13, 14, 15, 16), end=', ') # 48
-print(topDown(10, 11, 12, 13, 14, 15, 16, 17), end=', ') # 76
-print(topDown(10, 11, 12, 13, 14, 15, 16, 17, 18)) # 132
+print(dungeonNumber(10, mode='t'), end=', ') # 10
+print(dungeonNumber(10, 11, mode='t'), end=', ') # 11
+print(dungeonNumber(10, 11, 12, mode='t'), end=', ') # 13
+print(dungeonNumber(10, 11, 12, 13, mode='t'), end=', ') # 16
+print(dungeonNumber(10, 11, 12, 13, 14, mode='t'), end=', ') # 20 
+print(dungeonNumber(10, 11, 12, 13, 14, 15, mode='t'), end=', ') # 30
+print(dungeonNumber(10, 11, 12, 13, 14, 15, 16, mode='t'), end=', ') # 48
+print(dungeonNumber(10, 11, 12, 13, 14, 15, 16, 17, mode='t'), end=', ') # 76
+print(dungeonNumber(10, 11, 12, 13, 14, 15, 16, 17, 18, mode='t')) # 132
 
-print(topDown(10), end=', ') # 10
-print(topDown(11, 10), end=', ') # 11
-print(topDown(12, 11, 10), end=', ') # 13
-print(topDown(13, 12, 11, 10), end=', ') # 16
-print(topDown(14, 13, 12, 11, 10), end=', ') # 20
-print(topDown(15, 14, 13, 12, 11, 10), end=', ') # 28
-print(topDown(16, 15, 14, 13, 12, 11, 10), end=', ') # 45
-print(topDown(17, 16, 15, 14, 13, 12, 11, 10), end=', ') # 73
-print(topDown(18, 17, 16, 15, 14, 13, 12, 11, 10)) # 73
+print(dungeonNumber(10, mode='t'), end=', ') # 10
+print(dungeonNumber(11, 10, mode='t'), end=', ') # 11
+print(dungeonNumber(12, 11, 10, mode='t'), end=', ') # 13
+print(dungeonNumber(13, 12, 11, 10, mode='t'), end=', ') # 16
+print(dungeonNumber(14, 13, 12, 11, 10, mode='t'), end=', ') # 20
+print(dungeonNumber(15, 14, 13, 12, 11, 10, mode='t'), end=', ') # 28
+print(dungeonNumber(16, 15, 14, 13, 12, 11, 10, mode='t'), end=', ') # 45
+print(dungeonNumber(17, 16, 15, 14, 13, 12, 11, 10, mode='t'), end=', ') # 73
+print(dungeonNumber(18, 17, 16, 15, 14, 13, 12, 11, 10, mode='t')) # 133
