ce4f1841cd6b60d7d8263ebb0d6e0da420c7751f
thisusernameisaparadox <109701008+thisusernameisaparadox@users.noreply.github.com>
2022-12-25 13:35:52 -0500

    fully implemented

--- dungeon_numbers.py
+++ dungeon_numbers.py
@@ -20,62 +20,55 @@ def bottomUp(*args):
             return num
         
 def topDown(*args):
-    num = base(args[0], args[1])
-    args = list(args)
-    if (args):
-        args.pop(0)
-        if (len(args) > 1):
+    if len(args) == 1:
+        return args[0]
+    else:
+        num = base(args[0], args[1])
+        args = list(args)
+        if (args):
             args.pop(0)
-            return topDown(num, *tuple(args))
-        else:
-            return num
-    
+            if (len(args) > 1):
+                args.pop(0)
+                return topDown(num, *tuple(args))
+            else:
+                return num
 
-print("HERE: ")
-print(topDown(10, 11, 12, 13, 14, 15))
-print()
+print(bottomUp(10), end=', ') # 10
+print(bottomUp(10, 11), end=', ') # 11
+print(bottomUp(10, 11, 12), end=', ') # 13
+print(bottomUp(10, 11, 12, 13), end=', ') # 16
+print(bottomUp(10, 11, 12, 13, 14), end=', ') # 20
+print(bottomUp(10, 11, 12, 13, 14, 15), end=', ') # 25
+print(bottomUp(10, 11, 12, 13, 14, 15, 16), end=', ') # 31
+print(bottomUp(10, 11, 12, 13, 14, 15, 16, 17), end=', ') # 38
+print(bottomUp(10, 11, 12, 13, 14, 15, 16, 17, 18)) # 46
 
-print(bottomUp(10)) # 10
-print(bottomUp(10, 11)) # 11
-print(bottomUp(10, 11, 12)) # 13
-print(bottomUp(10, 11, 12, 13)) # 16
-print(bottomUp(10, 11, 12, 13, 14)) # 20
-print(bottomUp(10, 11, 12, 13, 14, 15)) # 25
+print(bottomUp(10), end=', ') # 10
+print(bottomUp(11, 10), end=', ') # 11
+print(bottomUp(12, 11, 10), end=', ') # 13
+print(bottomUp(13, 12, 11, 10), end=', ') # 16
+print(bottomUp(14, 13, 12, 11, 10), end=', ') # 20
+print(bottomUp(15, 14, 13, 12, 11, 10), end=', ') # 25
+print(bottomUp(16, 15, 14, 13, 12, 11, 10), end=', ') # 31
+print(bottomUp(17, 16, 15, 14, 13, 12, 11, 10), end=', ') # 38
+print(bottomUp(18, 17, 16, 15, 14, 13, 12, 11, 10)) # 46
 
-# print()
+print(topDown(10), end=', ') # 10
+print(topDown(10, 11), end=', ') # 11
+print(topDown(10, 11, 12), end=', ') # 13
+print(topDown(10, 11, 12, 13), end=', ') # 16
+print(topDown(10, 11, 12, 13, 14), end=', ') # 20 
+print(topDown(10, 11, 12, 13, 14, 15), end=', ') # 30
+print(topDown(10, 11, 12, 13, 14, 15, 16), end=', ') # 48
+print(topDown(10, 11, 12, 13, 14, 15, 16, 17), end=', ') # 76
+print(topDown(10, 11, 12, 13, 14, 15, 16, 17, 18)) # 132
 
-# print(bottomUp(11, 10)) # 11
-# print(bottomUp(12, 11, 10)) # 13
-# print(bottomUp(13, 12, 11, 10)) # 16
-# print(bottomUp(14, 13, 12, 11, 10)) # 20
-# print(bottomUp(15, 14, 13, 12, 11, 10)) # 25
-
-print()
-# Ascending from the bottom up
-# 11
-print(base(10, 11))
-# 13
-print(base(base(10, 11), 12))
-# 16
-print(base(base(base(10, 11), 12), 13))
-# 20
-print(base(base(base(base(10, 11), 12), 13), 14))
-# 30
-print(base(base(base(base(base(10, 11), 12), 13), 14), 15))
-
-print()
-
-
-# print()
-# print(topDown(10, 11)) # 11
-# print(topDown(10, 11, 12)) # 13
-# print(topDown(10, 11, 12, 13)) # 16
-# print(topDown(10, 11, 12, 13, 14)) # 20 
-# print(topDown(10, 11, 12, 13, 14, 15)) # 30
-
-# print()
-# print(topDown(11, 10)) # 11
-# print(topDown(12, 11, 10)) # 13
-# print(topDown(13, 12, 11, 10)) # 16
-# print(topDown(14, 13, 12, 11, 10)) # 20
-# print(topDown(15, 14, 13, 12, 11, 10)) # 28
+print(topDown(10), end=', ') # 10
+print(topDown(11, 10), end=', ') # 11
+print(topDown(12, 11, 10), end=', ') # 13
+print(topDown(13, 12, 11, 10), end=', ') # 16
+print(topDown(14, 13, 12, 11, 10), end=', ') # 20
+print(topDown(15, 14, 13, 12, 11, 10), end=', ') # 28
+print(topDown(16, 15, 14, 13, 12, 11, 10), end=', ') # 45
+print(topDown(17, 16, 15, 14, 13, 12, 11, 10), end=', ') # 73
+print(topDown(18, 17, 16, 15, 14, 13, 12, 11, 10)) # 73
