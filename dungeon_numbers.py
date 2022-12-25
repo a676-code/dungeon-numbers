767e85f0c0820dc990d285bf2e1f6ee014332c43
thisusernameisaparadox <109701008+thisusernameisaparadox@users.noreply.github.com>
2022-12-24 21:13:11 -0500

    partial implementation

--- dungeon_numbers.py
+++ dungeon_numbers.py
@@ -8,23 +8,52 @@ def base(a, b):
         result += d * (int(b) ** i)
     return result
 
-# Descending from the bottom up
-# 11
-print(base(10, 11))
-# 13
-print(base(10, base(11, 12)))
-# 16
-print(base(10, base(11, base(12, 13))))
-# 20
-print(base(10, base(11, base(12, base(13, 14)))))
-# 25
-print(base(10, base(11, base(12, base(13, base(14, 15))))))
+def bottomUp(*args):
+    num = base(args[len(args) - 2], args[len(args) - 1])
+    args = list(args)
+    if (args):
+        args.pop(len(args) - 1)
+        if (len(args) > 1):
+            args.pop(len(args) - 1)
+            return bottomUp(*tuple(args), num)
+        else:
+            return num
+        
+def topDown(*args):
+    num = base(args[0], args[1])
+    args = list(args)
+    if (args):
+        args.pop(0)
+        if (len(args) > 1):
+            args.pop(0)
+            return topDown(num, *tuple(args))
+        else:
+            return num
+    
 
+print("HERE: ")
+print(topDown(10, 11, 12, 13, 14, 15))
 print()
 
+print(bottomUp(10)) # 10
+print(bottomUp(10, 11)) # 11
+print(bottomUp(10, 11, 12)) # 13
+print(bottomUp(10, 11, 12, 13)) # 16
+print(bottomUp(10, 11, 12, 13, 14)) # 20
+print(bottomUp(10, 11, 12, 13, 14, 15)) # 25
+
+# print()
+
+# print(bottomUp(11, 10)) # 11
+# print(bottomUp(12, 11, 10)) # 13
+# print(bottomUp(13, 12, 11, 10)) # 16
+# print(bottomUp(14, 13, 12, 11, 10)) # 20
+# print(bottomUp(15, 14, 13, 12, 11, 10)) # 25
+
+print()
 # Ascending from the bottom up
 # 11
-print(base(11, 10))
+print(base(10, 11))
 # 13
 print(base(base(10, 11), 12))
 # 16
@@ -36,28 +65,17 @@ print(base(base(base(base(base(10, 11), 12), 13), 14), 15))
 
 print()
 
-# Descending from the bottom up
-#11
-print(base(11, 10))
-# 13
-print(base(12, base(11, 10)))
-# 16
-print(base(13, base(12, base(11, 10))))
-# 20
-print(base(14, base(13, base(12, base(11, 10)))))
-# 25
-print(base(15, base(14, base(13, base(12, base(11, 10))))))
 
-print()
+# print()
+# print(topDown(10, 11)) # 11
+# print(topDown(10, 11, 12)) # 13
+# print(topDown(10, 11, 12, 13)) # 16
+# print(topDown(10, 11, 12, 13, 14)) # 20 
+# print(topDown(10, 11, 12, 13, 14, 15)) # 30
 
-# Descending from the top down
-# 11
-print(base(11, 10))
-# 13
-print(base(base(12, 11), 10))
-# 16
-print(base(base(base(13, 12), 11), 10))
-# 20
-print(base(base(base(base(14, 13), 12), 11), 10))
-# 28
-print(base(base(base(base(base(15, 14), 13), 12), 11), 10))
+# print()
+# print(topDown(11, 10)) # 11
+# print(topDown(12, 11, 10)) # 13
+# print(topDown(13, 12, 11, 10)) # 16
+# print(topDown(14, 13, 12, 11, 10)) # 20
+# print(topDown(15, 14, 13, 12, 11, 10)) # 28
